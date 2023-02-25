b3b01f7f270c9cf960cb9bb4099a22c39aa58d75
chandinithirumalai <chandini607@gmail.com>
2023-02-25 19:25:24 +0800

    Changes made to App.jsx, LoginForm.jsx and Nav.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
+
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-
 // Pages
-import HomePage from "./Pages/HomePage";
-import LoginPage from "./Pages/LoginPage";
-import ProjectPage from "./Pages/ProjectPage";
-
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProjectPage from "./pages/ProjectPage";
 // Components
-import Nav from "./Components/Nav/Nav";
-import "./App.css"
-const HeaderLayout = () => (
-  <div>
-    <Nav />
-    <Outlet />
-  </div>
-);
-
-
+import Nav from "./components/Nav/Nav";
+import Footer from "./components/Footer/Footer";
+// CSS
+import "./App.css";
+import { useState } from "react";
+const Layout = () => {
+  const [loggedIn, setLoggedIn] = useState(window.localStorage.getItem("token") != null)
+  return (
+    <>
+      <Nav loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+      <Outlet context={[loggedIn, setLoggedIn]} />
+      <Footer />
+    </>
+  );
+}
 const router = createBrowserRouter([
   {
-    element: <HeaderLayout />,
+    element: <Layout />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/login", element: <LoginPage /> },
@@ -26,9 +30,7 @@ const router = createBrowserRouter([
     ],
   },
 ]);
-
 function App() {
   return <RouterProvider router={router} />;
 }
-
 export default App;
